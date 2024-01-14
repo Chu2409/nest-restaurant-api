781d7d8cd3bc41c39da5f6da09d6398a69e99dc5
Daniel Zhu <dzhu2409@gmail.com>
2024-01-13 20:14:30 -0500

    Extend employees services

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,13 +1,51 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { LoginEmployeeDto } from './dto/login-employee.dto';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post('login')
-  login(@Body() loginEmployeeDto: LoginEmployeeDto) {
-    return this.employeesService.login(loginEmployeeDto);
+  async login(@Body() loginEmployeeDto: LoginEmployeeDto) {
+    return await this.employeesService.login(loginEmployeeDto);
+  }
+
+  @Post()
+  async create(@Body() createEmployeeDto: CreateEmployeeDto) {
+    return await this.employeesService.create(createEmployeeDto);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.employeesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.employeesService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateEmployeeDto: UpdateEmployeeDto,
+  ) {
+    return await this.employeesService.update(id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this.employeesService.remove(id);
   }
 }
